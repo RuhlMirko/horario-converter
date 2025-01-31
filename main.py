78f2580f235e3b48f8030594f77e7c3f8125346c
Mirko <ruhlmirkojoel@gmail.com>
2025-01-31 13:07:01 -0300

    Ui added

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-import ttkbootstrap as ttk
+import ttkbootstrap as tb
 from ttkbootstrap.constants import *
-
-root = ttk.Window(themename='darkly')
+# root
+root = tb.Window(themename='darkly')
 root.geometry('500x400')
 
-pdf_frame = ttk.Frame(root, relief='sunken')
-pdf_frame.pack(pady=50)
+# styles
+Style = tb.Style()
+Style.configure('TLabel', font=('Segoe UI', 14, 'bold'))
+Style.configure('TButton', font=('Segoe UI', 10))
 
-pdf_path_entry = ttk.Entry(pdf_frame, style='info', width=70)
-pdf_path_entry.pack()
+# Title
+title_lbl = tb.Label(text='PDF Converter')
+title_lbl.pack(pady=30)
+pdf_frame = tb.Frame(root)
+pdf_frame.pack(pady=10)
+
+# Widgets
+pdf_path_entry = tb.Entry(pdf_frame, style='info', width=70)
+pdf_path_entry.grid(row=0, column=0, columnspan=2, pady=10)
+
+search_btn = tb.Button(pdf_frame, text='Search file', width=20)
+search_btn.grid(row=1, column=0)
+convert_btn = tb.Button(pdf_frame, text='Convert pdf', width=20, style='outline-info')
+convert_btn.grid(row=1, column=1)
+
+
+
 
 root.mainloop()
